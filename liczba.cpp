26ac760e6dd4c1d7f191aa1c5ecfdab8c00c66f0
Pawel Witkowski <pawwit@mat.umk.pl>
2024-12-28 17:20:56 +0100

    Add function to check perfect numbers

--- liczba.cpp
+++ liczba.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-int main() {
-    int liczba;
-    std::cin >> liczba;
-    std::cout << liczba << std::endl;
-    return 0;
-}
+bool czy_doskonala(int liczba) {
+    int suma = 0;
+        for (int i = 1; i < liczba; ++i) {
+                if (liczba % i == 0) suma += i;
+                    }
+                        return suma == liczba;
+                        }
+                        int main() {
+                            int liczba;
+                                std::cin >> liczba;
+                                    std::cout << liczba << " jest ";
+                                        if (!czy_doskonala(liczba)) std::cout << "nie ";
+                                            std::cout << "liczbą doskonałą" << std::endl;
+                                                return 0;
+                                                }
+                                                
