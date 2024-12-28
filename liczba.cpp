0f0e4808054aaaaade80e7078086aaa3b6bb1a29
Pawel Witkowski <pawwit@mat.umk.pl>
2024-12-28 17:28:20 +0100

    Add check for positive natural numbers

--- liczba.cpp
+++ liczba.cpp
@@ -2,6 +2,10 @@
 int main() {
     int liczba;
     std::cin >> liczba;
+    if (liczba <= 0) {
+            std::cout << "Podana liczba nie jest dodatnią liczbą naturalną." << std::endl;
+                    return 1;
+                        }
     std::cout << liczba << std::endl;
     return 0;
 }
